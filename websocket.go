1f5624c63375a837fd72c88e9b04be7b6598898f
peterksts <peterk@silvertreesystems.com>
2019-08-07 20:29:38 +0300

    refactoring tickers ws

--- websocket.go
+++ websocket.go
@@ -19,15 +19,18 @@ func newWsConfig(endpoint string) *wsConfig {
 
 type wsServeFunc func(*wsConfig, WsHandler) (chan struct{}, error)
 
-var wsServe = func(cfg *wsConfig, handler WsHandler) (done chan struct{}, err error) {
+func wsServe(cfg *wsConfig, handler WsHandler) (done chan struct{}, err error) {
 	c, _, err := websocket.DefaultDialer.Dial(cfg.endpoint, nil)
 	if err != nil {
 		return
 	}
-	done = make(chan struct{})
+	done = make(chan struct{}, 2)
 	go func() {
-		defer c.Close()
-		defer close(done)
+		defer func() {
+			_ = c.Close()
+			close(done)
+		}()
+
 		for {
 			_, message, err := c.ReadMessage()
 			if err != nil {
@@ -36,5 +39,51 @@ var wsServe = func(cfg *wsConfig, handler WsHandler) (done chan struct{}, err er
 			go handler(message)
 		}
 	}()
+
+	return
+}
+
+func wsServeMax(cfg *wsConfig, handler WsHandler, done chan interface{}) (lsDone chan interface{}, err error) {
+	c, _, err := websocket.DefaultDialer.Dial(cfg.endpoint, nil)
+	if err != nil {
+		return
+	}
+
+	if done == nil {
+		done = make(chan interface{}, 2)
+	}
+	lsDone = make(chan interface{}, 2)
+
+	go func() {
+		v := <-done
+		lsDone <- v
+		if v != nil {
+			c.Close()
+		}
+	}()
+
+	go func() {
+		defer func() {
+			_ = c.Close()
+
+			if pan := recover(); pan != nil {
+				go wsServeMax(cfg, handler, done)
+				return
+			}
+
+			close(lsDone)
+			close(done)
+		}()
+
+		for {
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				return
+			}
+
+			go handler(message)
+		}
+	}()
+
 	return
 }
--- websocket_orderbook.go
+++ websocket_orderbook.go
@@ -12,13 +12,15 @@ type WsPartialBookDepthHandler func(event *WsPartialBookDepthEvent)
 
 // WsPartialBookDepthServe serve websocket depth handler with a symbol.
 // Valid <levels> are 5, 10, or 20.
-func WsPartialBookDepthServe(symbol string, handler WsPartialBookDepthHandler, levels int) (chan struct{}, error) {
+func WsPartialBookDepthServe(symbol string, handler WsPartialBookDepthHandler, levels int) (done chan interface{}, lsDone chan interface{}, err error) {
 	endpoint := fmt.Sprintf("%s/%s@depth%d", baseURL, strings.ToLower(symbol), levels)
 	cfg := newWsConfig(endpoint)
 	wsHandler := func(message []byte) {
 		handler(parseWsPartialBookDepthEvent(message, levels))
 	}
-	return wsServe(cfg, wsHandler)
+	done = make(chan interface{}, 2)
+	lsDone, err = wsServeMax(cfg, wsHandler, done)
+	return
 }
 
 // WsPartialBookDepthEvent define websocket depth event
